548842d58c0b100da93c6725ce53bbaac80daecb
Hrithik <161672570+GaLactic-Acid@users.noreply.github.com>
2024-12-22 17:27:21 +1100

    Update MyFrame.java

--- Snake game/src/MyFrame.java
+++ Snake game/src/MyFrame.java
@@ -1,9 +1,4 @@
 import javax.swing.JFrame;
-//import java.awt.Color;
-// import java.awt.Graphics;
-// import java.awt.Graphics2D;
-
-import java.awt.Color;
 
 public class MyFrame extends JFrame {
     MyPanel panel;
