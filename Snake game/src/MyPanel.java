e66d277632e7edd7ef0c6feeb58aae221ef37348
Hrithik <161672570+GaLactic-Acid@users.noreply.github.com>
2024-12-25 14:51:22 +1100

    a square (rect) that can move
    added a rectangle to mimic snake that can move up, down ,left, right with the respective 'WASD" keys. Added actionlistener and keylistener to do this.

--- Snake game/src/MyPanel.java
+++ Snake game/src/MyPanel.java
@@ -2,39 +2,77 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.Dimension;
+import javax.swing.Timer;
 
-public class MyPanel extends JPanel implements KeyListener {
-  int PANEL_WIDTH = 800;
-  int PANEL_HEIGHT = 800;
+public class MyPanel extends JPanel implements KeyListener, ActionListener {
+  int PANEL_WIDTH = 600;
+  int PANEL_HEIGHT = 600;
   Pellet pellet;
+  Snake snake;
+  Timer timer;
 
   MyPanel() {
     this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
     this.setBackground(Color.BLACK);
     pellet = new Pellet(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    snake = new Snake(PANEL_WIDTH/2, PANEL_HEIGHT/2, pellet);
     this.addKeyListener(this);
     this.setFocusable(true);
     this.requestFocusInWindow();
-  }
 
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    Graphics2D pelletG2D = (Graphics2D) g;
-    pellet.paint(pelletG2D);
-  }
-
+  timer = new Timer(10, new ActionListener(){
   @Override
-  public void keyPressed(KeyEvent e) {
-    if(e.getKeyCode() == KeyEvent.VK_W) {
-      pellet.pelletEaten = true;
-      pellet.pelletCoords();
-      repaint();    
+  public void actionPerformed(ActionEvent e) {
+      if (snake.direction == 0) {
+          snake.SnakeUp();
+      } else if (snake.direction == 1) {
+          snake.SnakeDown();
+      } else if (snake.direction == 2) {
+          snake.SnakeLeft();
+      } else if (snake.direction == 3) {
+          snake.SnakeRight();
+      }
+      repaint();
     }
-  }
+  });
+  timer.start();
+}
 
+public void paintComponent(Graphics g) {
+  super.paintComponent(g);
+  Graphics2D snakeG2D = (Graphics2D) g;
+  Graphics2D pelletG2D = (Graphics2D) g;
+  pellet.paint(pelletG2D);
+  snake.paint(snakeG2D);
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+  // TODO Auto-generated method stub
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+    if (e.getKeyCode() == KeyEvent.VK_W) {
+        snake.direction = 0;
+        System.out.println("Snake moved up");
+    } else if (e.getKeyCode() == KeyEvent.VK_S) {
+        snake.direction = 1;
+        System.out.println("Snake moved down");
+    } else if (e.getKeyCode() == KeyEvent.VK_A) {
+        snake.direction = 2;
+        System.out.println("Snake moved left");
+    } else if (e.getKeyCode() == KeyEvent.VK_D) {
+        snake.direction = 3;
+        System.out.println("Snake moved right");
+    }
+      repaint();
+}
   @Override
   public void keyReleased(KeyEvent e) {
     // TODO Auto-generated method stub
@@ -44,6 +82,4 @@ public class MyPanel extends JPanel implements KeyListener {
   public void keyTyped(KeyEvent e) {
     // TODO Auto-generated method stub
   }
-
-  
 }
--- Snake game/src/Snake.java
+++ Snake game/src/Snake.java
@@ -1,16 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
 public class Snake {
-    public int snakeLength;
-    public int snakeDirection; // 0 = up, 1 = right, 2 = down, 3 = left
-    public int snakeSpeed;
     public int snakeX;
     public int snakeY;
+    public Pellet pellet;
+    public int speed=4; // to change snake speed
+    public int direction = 3; // 0 = up, 1 = down, 2 = left, 3 = right
 
     // add position of snake tail as attribute
-    public Snake(int snakeX, int snakeY, int snakeLength, int snakeDirection, int snakeSpeed) {
+    public Snake(int snakeX, int snakeY, Pellet pellet) {
+        this.pellet = pellet;
         this.snakeX = snakeX;
         this.snakeY = snakeY;
-        this.snakeLength = snakeLength;
-        this.snakeDirection = snakeDirection;
-        this.snakeSpeed = snakeSpeed;
     };
+
+    public void paint(Graphics2D snakeG2D) {
+        snakeG2D.setColor(Color.white);
+        snakeG2D.fillRect(snakeX,snakeY,20,20);
+    }
+
+    public void SnakeRight() {
+
+        this.snakeX+=speed;
+    }
+
+    public void SnakeLeft() {
+        this.snakeX-=speed;
+    }
+
+    public void SnakeUp() {
+        this.snakeY-=speed;
+    }
+
+    public void SnakeDown() {
+        this.snakeY+=speed;
+    }
 }
