d3984cd6b92cfacabe753608a02bc915cfb701f5
Hrithik <161672570+GaLactic-Acid@users.noreply.github.com>
2024-12-25 15:00:33 +1100

    added panelSize as reference and checkbounds
    checkbounds allows the snake to remain in the window coming from the opposite end if it exceeds boundary

--- Snake game/src/Snake.java
+++ Snake game/src/Snake.java
@@ -1,14 +1,19 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Dimension;
 public class Snake {
     public int snakeX;
     public int snakeY;
     public Pellet pellet;
     public int speed=4; // to change snake speed
     public int direction = 3; // 0 = up, 1 = down, 2 = left, 3 = right
+    int snakeWidth = 20;
+    int snakeHeight = 20;
+    Dimension panelSize;
 
     // add position of snake tail as attribute
-    public Snake(int snakeX, int snakeY, Pellet pellet) {
+    public Snake(int snakeX, int snakeY, Pellet pellet, Dimension panelSize) {
+        this.panelSize = panelSize;
         this.pellet = pellet;
         this.snakeX = snakeX;
         this.snakeY = snakeY;
@@ -16,7 +21,7 @@ public class Snake {
 
     public void paint(Graphics2D snakeG2D) {
         snakeG2D.setColor(Color.white);
-        snakeG2D.fillRect(snakeX,snakeY,20,20);
+        snakeG2D.fillRect(snakeX,snakeY,snakeWidth,snakeHeight);
     }
 
     public void SnakeRight() {
@@ -35,4 +40,18 @@ public class Snake {
     public void SnakeDown() {
         this.snakeY+=speed;
     }
+public void checkBounds() {
+    if(snakeX<0-snakeWidth) {
+        snakeX = panelSize.width;
+    }
+    if(snakeX>panelSize.width) {
+        snakeX = 0;
+    }
+    if(snakeY<0-snakeHeight) {
+        snakeY = panelSize.height;
+    }
+    if(snakeY>panelSize.height) {
+        snakeY = 0;
+    }
+}
 }
