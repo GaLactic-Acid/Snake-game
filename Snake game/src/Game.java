0f7e34595e63954b66989032f20ff90c5fef2d66
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-02-20 11:10:20 +1100

    Update Game.java

--- Snake game/src/Game.java
+++ Snake game/src/Game.java
@@ -1,6 +1,6 @@
 public class Game {
-    //I did something W !!1!!11
     public static void main(String[] args) throws Exception {
         new MyFrame();
+        //I did something W !!1!!11
     }
 }
