14e59ee912ddba8a1db5c4d34e6f3513c62668d3
Hrithik <161672570+GaLactic-Acid@users.noreply.github.com>
2025-04-11 14:07:52 +1000

    Update Game.java

--- Snake game/src/Game.java
+++ Snake game/src/Game.java
@@ -1,6 +1,6 @@
 public class Game {
     public static void main(String[] args) throws Exception {
-        new MyFrame();
+        new MyFrame(); //starts game (MyFrame -> GamePanel -> Snake + Pellet)
         //I did something W !!1!!11 - airstriker
     }
 }
